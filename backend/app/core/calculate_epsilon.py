04d2d32253835429f5a0110adc45d26911c9dbd4
Blank36 <nikolaenkoyu36@gmail.com>
2026-05-10 20:47:02 +0300

    исправление расчета терапии

--- backend/app/core/calculate_epsilon.py
+++ backend/app/core/calculate_epsilon.py
@@ -1,15 +1,12 @@
 from math import exp, pi, sin
-from ..model.TherapyModes import TherapyModes
 
 def calculate_epsilon(e0, mode, gamma, t):
-    if mode == TherapyModes.THERAPY:
+    if mode == 'THERAPY':
         return e0 if t > gamma else 0.0
-
-    if mode == TherapyModes.INTERRUPTION:
+    elif mode == 'INTERRUPTION':
         frequency = 2 * pi / gamma
         return e0 * (sin(frequency * t) + 1) / 2
-
-    if mode == TherapyModes.RESISTANCE:
+    elif mode == 'RESISTANCE':
         return e0 * exp(-gamma * t)
-
-    return 0.0
+    else:
+        return 0.0
